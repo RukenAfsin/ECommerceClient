0ccb0a6ceb8cc7dce86515fb573ee9d30ac1a38f
Ruken Afşin <rukenafsin13@gmail.com>
2024-01-16 15:56:50 +0300

    Modules and Components Created

---
+++ src/app/admin/components/components.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductsModule } from './products/products.module';
+import { OrderModule } from './order/order.module';
+import { CustomerModule } from './customer/customer.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { HomeModule } from '../../ui/components/home/home.module';
+import { BasketsModule } from '../../ui/components/baskets/baskets.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ProductsModule,
+    OrderModule,
+    CustomerModule,
+    HomeModule,
+    BasketsModule,
+    DashboardModule
+  ]
+})
+export class ComponentsModule { }
