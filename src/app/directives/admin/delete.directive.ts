0617865c6af1e89b34985bf6e0ca944f8c53b538
Ruken Afşin <rukenafsin13@gmail.com>
2024-02-07 19:21:07 +0300

    centralization of dialogue pages

--- src/app/directives/admin/delete.directive.ts
+++ src/app/directives/admin/delete.directive.ts
@@ -5,6 +5,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { DeleteDialogComponent, DeleteState } from '../../dialogs/delete-dialog/delete-dialog.component';
 import { AlertifyService, MessageType, Position } from '../../services/admin/alertify.service';
 import { HttpErrorResponse } from '@angular/common/http';
+import { DialogService } from '../../services/common/dialog.service';
 
 declare var $:any;
 
@@ -18,7 +19,8 @@ export class DeleteDirective {
     private _renderer: Renderer2,
    private httpClientService:HttpClientService,
    public dialog: MatDialog,
-   private alertifyService: AlertifyService) {
+   private alertifyService: AlertifyService,
+   private dialogService:DialogService) {
 
 
       const img=_renderer.createElement("img")
@@ -36,48 +38,52 @@ export class DeleteDirective {
 
 @HostListener("click")
     async  onclick(){
-      this.openDialog(async ()=>{
-        const td:HTMLTableCellElement=this.element.nativeElement
-        this.httpClientService.delete({
-          controller:this.controller,
-        },this.id).subscribe(data=>{
-          $(td.parentElement).animate({
-            opacity:0,
-            left:"+=50",
-            height:"toogle"
-           },700,()=>{
-            this.callback.emit();
-            this.alertifyService.message("Deleted successfully",{
+      this.dialogService.openDialog({
+        componentType:DeleteDialogComponent,
+        data:DeleteState.Yes,
+        afterClosed:async ()=>{
+          const td:HTMLTableCellElement=this.element.nativeElement
+          this.httpClientService.delete({
+            controller:this.controller,
+          },this.id).subscribe(data=>{
+            $(td.parentElement).animate({
+              opacity:0,
+              left:"+=50",
+              height:"toogle"
+             },700,()=>{
+              this.callback.emit();
+              this.alertifyService.message("Deleted successfully",{
+                dismissOthers:true,
+                messageType:MessageType.Success,
+                position:Position.TopRight
+              })
+             })        
+             .fadeOut(2000, ()=>{
+          });  
+          }, (errorResponse:HttpErrorResponse)=>{
+            this.alertifyService.message("Deleted error",{
               dismissOthers:true,
-              messageType:MessageType.Success,
+              messageType:MessageType.Error,
               position:Position.TopRight
             })
-           })        
-           .fadeOut(2000, ()=>{
-        });  
-        }, (errorResponse:HttpErrorResponse)=>{
-          this.alertifyService.message("Deleted error",{
-            dismissOthers:true,
-            messageType:MessageType.Error,
-            position:Position.TopRight
-          })
-        })  
+          })  
+        }
       })   
      }
 
 
 
-     openDialog(afterClosed:any): void {
-      const dialogRef = this.dialog.open(DeleteDialogComponent, {
-        width:'250px',
-        data: DeleteState.Yes,
-      });
+    //  openDialog(afterClosed:any): void {
+    //   const dialogRef = this.dialog.open(DeleteDialogComponent, {
+    //     width:'250px',
+    //     data: DeleteState.Yes,
+    //   });
   
-      dialogRef.afterClosed().subscribe(result => {
-       if(result==DeleteState.Yes)
-       afterClosed()
+    //   dialogRef.afterClosed().subscribe(result => {
+    //    if(result==DeleteState.Yes)
+    //    afterClosed()
        
-      });
-    }
+    //   });
+    // }
 
 }
