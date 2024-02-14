2d8bd1ae3faeab7d83cd781ae8173a5c222dd9bb
Ruken Afşin <rukenafsin13@gmail.com>
2024-02-14 12:08:03 +0300

    Register page completed with Reactive Forms

---
+++ src/app/entities/user.ts
@@ -0,0 +1,7 @@
+export class User{
+    namesurname:string;
+    name:string;
+    email:string;
+    password:string;
+    passwordconfirm:string;
+}
--- src/app/ui/components/register/register.component.ts
+++ src/app/ui/components/register/register.component.ts
@@ -1,43 +1,59 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
+import { User } from '../../../entities/user';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule,FormsModule,CommonModule],
+  imports: [ReactiveFormsModule, FormsModule, CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
 
-  constructor(private formBuilder:FormBuilder){
+  constructor(private formBuilder: FormBuilder) {
 
-    this.frm=this.formBuilder.group({
-      namesurname:["",[Validators.required,Validators.maxLength(50),Validators.minLength(3)]],
-      username:["",[Validators.required]],
-      email:["",[Validators.required,Validators.email]],
-      password:[""],
-      passwordconfirm:[""]
-    })
+    this.frm = this.formBuilder.group({
+      namesurname: ["", [Validators.required, Validators.maxLength(50),
+      Validators.minLength(3)]],
+      username: ["", [Validators.required]],
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", 
+      [Validators.required]],
+      passwordconfirm: ["", 
+     [Validators.required
+    ]]
+    },{
+
+      validators:(group:AbstractControl): ValidationErrors|null=>{
+        let password=group.get("password").value;
+        let passwordconfirm =group.get("passwordconfirm").value;  
+        return password === passwordconfirm?null:{notSame:true}
+      }
+    }
+    )
+
   }
 
-  frm:FormGroup;
+  frm: FormGroup;
 
-get component(){
-  return this.frm.controls;
-}
-
-submitted:boolean=false;
-  onSubmit(data:any){
-  this.submitted=true;
-var c =this.component;
-
-
-
-  if(this.frm.invalid)
-  return;
-    debugger;
+  get component() {
+    return this.frm.controls;
   }
- 
+
+  submitted: boolean = false;
+  onSubmit(data: User) {
+    this.submitted = true;
+    // var c = this.component;
+    // var f= this.frm;
+    // var d= this.frm.hasError("notSame")
+
+
+    if (this.frm.invalid)
+      return;
+  }
+
+
+
 }
